4b11f3c4b09ca0c298563e27f3625c13e4cb02d7
Hector <915106059@qq.com>
2022-04-22 21:31:35 +0800

    hot-fix commit1

--- src/main/java/com/dengwei/git/GitTest.java
+++ src/main/java/com/dengwei/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("hello git！");
+        System.out.println("hot-fix commit1");
     }
 }
